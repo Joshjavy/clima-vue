3a6e0b9aab661c3e2252c247c6d3e86133ef5d7c
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-02-26 17:57:13 -0600

    173. Extrayendo Valores de la consulta previa

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,11 +1,22 @@
+import axios from "axios";
 export default function useClima(){
     
-    const obtenerClima=({ ciudad, pais})=>{
+    const obtenerClima= async({ ciudad, pais})=>{
         //importar apikey
         const key =import.meta.env.VITE_API_KEY;
-        //obtener la lat, lngt
-        const ur =`http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}
-                    &limit=1&appid=${key}`;
+        try {
+            //obtener la lat, lngt
+            const url =`http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}
+            &limit=1&appid=${key}`;
+            const { data} = await axios (url);
+            const {lat,lon}=data[0];
+            
+            
+
+        } catch (error) {
+            console.log(error);
+        }
+        
         
         //obtener el clima
     }
