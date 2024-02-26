c942f3768ff502c74ef03a2eb0d033b299b786e4
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-02-26 12:23:11 -0600

    166. Creando el Formulario Principal

---
+++ src/Formulario.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { initCustomFormatter } from 'vue';
+
+const paises = [
+    { codigo: 'US', nombre: 'Estados Unidos' },
+    { codigo: 'MX', nombre: 'México' },
+    { codigo: 'AR', nombre: 'Argentina' },
+    { codigo: 'CO', nombre: 'Colombia' },
+    { codigo: 'CR', nombre: 'Costa Rica' },
+    { codigo: 'ES', nombre: 'España' },
+    { codigo: 'PE', nombre: 'Perú' }
+]
+</script>
+<template>
+    <form class="formulario">
+        <div class="campo">
+            <label for="ciudad">Ciudad</label>
+            <input type="text" id="ciudad" placeholder="Ciudad" class="" />
+        </div>
+
+        <div class="campo">
+            <label for="pais">pais</label>
+            <select id="pais">
+                <option value=""> -- Seleccione un pais -- </option>
+                <option 
+                v-for="pais in paises"
+                :value="pais.codigo"
+                >
+                    {{ pais.nombre }}
+                </option>
+            </select>
+        </div>
+        <input type="submit" value="Consultar Clima">
+    </form>
+</template>
